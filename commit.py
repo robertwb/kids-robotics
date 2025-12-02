4017beaff730d9b601f22023792adc2f07226f08
Robert Bradshaw <robertwb@gmail.com>
2025-12-01 23:02:36 -0800

    Allow explicit setting of date.

--- commit.py
+++ commit.py
@@ -9,6 +9,7 @@ import format
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('filename')
+  parser.add_argument('--date')
   args = parser.parse_args()
 
   path = args.filename
@@ -17,4 +18,6 @@ if __name__ == '__main__':
   basename = re.sub(r' [(]\d+[)]', '', os.path.basename(path)) + '.yaml'
   with open(basename, 'w') as fout:
     fout.write(content)
-  subprocess.check_call(['git', 'commit', basename])
+  subprocess.check_call(
+      ['git', 'commit', basename],
+      env={**os.environ, **{'GIT_AUTHOR_DATE': args.date, 'GIT_COMMITTER_DATE': args.date}})
